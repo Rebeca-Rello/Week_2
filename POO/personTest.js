e60c20ce247dd2399d6c9deaef2d84023bfe42ca
Rebeca Rello <rebeca.rello@gmail.com>
2023-02-21 13:10:51 +0100

    person modificaion

--- POO/personTest.js
+++ POO/personTest.js
@@ -4,23 +4,23 @@
 const {Persona}= require("./person")
 
 
-// console.log('*******METER VALORES EN PARAMETRO******');
+console.log('*******METER VALORES EN PARAMETRO******');
 
-// let mario= new Persona(70,1.80, "verdes", "rubio", 1980, "nadar bailar leer");
-// console.log(mario);
+let mario= new Persona(70,1.80, "verdes", "rubio", 1980, "nadar bailar leer");
+console.log(mario);
 
-// console.log('*******CALCULAR IMC******');
+console.log('*******CALCULAR IMC******');
 
-// console.log(mario.calcularIMC());
+console.log(mario.calcularIMC());
 
-// console.log('*******CALCULAR EDAD******');
+console.log('*******CALCULAR EDAD******');
 
-// console.log(mario.edad(2023));
+console.log(mario.edad(2023));
 
-// console.log('*******HOBBIES******');
+console.log('*******HOBBIES******');
 
-// console.log(mario.printHobbies());
+console.log(mario.printHobbies());
 
-// console.log('*******PRINT ALL******');
+console.log('*******PRINT ALL******');
 
-// mario.printAll()
+mario.printAll()
